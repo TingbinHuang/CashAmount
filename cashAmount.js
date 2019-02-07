3532b483f665b259bab18f369918e43eac00f680
Tingbin Huang <tingbinhuang@Tingbins-MacBook-Pro.local>
2019-02-07 09:45:51 -0800

    Updataed

--- cashAmount.js
+++ cashAmount.js
@@ -3,6 +3,7 @@ class CashAmount {
 	constructor(amount) {
 		this.amount = amount; 
 		this.currentAmount = amount; 
+		this.obj = {};
 	}
 
 	current() { // function for debugging 
@@ -26,10 +27,18 @@ class CashAmount {
 	}
 
 	quantityOfEachDenomination() {
+		let len = (Math.floor(this.currentAmount)).toString().length;
+
+		// I assume the maximum amount is up to 'hundreds'
+
+		this.obj.hundreds = Math.floor(len / 100); 
 
 	}
 
 }
 
+const cash = new CashAmount(967.93);
+
+console.log(cash.quantityOfEachDenomination());
 
 
